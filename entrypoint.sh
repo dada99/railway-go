ea434ae48432545dc41213deda529d094f0ab2a5
dada99 <dada99@163.com>
2022-09-04 22:01:10 +0800

    First commit

---
+++ entrypoint.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+#版本
+if [[ -z "${VER}" ]]; then
+  VER="latest"
+fi
+echo ${VER}
+
+if [[ -z "${Vless_Path}" ]]; then
+  Vless_Path="/s233"
+fi
+echo ${Vless_Path}
+
+if [[ -z "${Vless_UUID}" ]]; then
+  Vless_UUID="5c301bb8-6c77-41a0-a606-4ba11bbab084"
+fi
+echo ${Vless_UUID}
+
+if [[ -z "${Vmess_Path}" ]]; then
+  Vmess_Path="/s244"
+fi
+echo ${Vmess_Path}
+
+if [[ -z "${Vmess_UUID}" ]]; then
+  Vmess_UUID="5c301bb8-6c77-41a0-a606-4ba11bbab084"
+fi
+echo ${Vmess_UUID}
+
+if [[ -z "${Share_Path}" ]]; then
+  Share_Path="/share233"
+fi
+echo ${Share_Path}
+
+chmod +x /mybin/mygo
+ls -al
+
+cd /wwwroot
+tar xvf wwwroot.tar.gz
+rm -rf wwwroot.tar.gz
+
+
+sed -e "/^#/d"\
+    -e "s/\${Vless_UUID}/${Vless_UUID}/g"\
+    -e "s|\${Vless_Path}|${Vless_Path}|g"\
+    -e "s/\${Vmess_UUID}/${Vmess_UUID}/g"\
+    -e "s|\${Vmess_Path}|${Vmess_Path}|g"\
+    /conf/mygo.template.json >  /mybin/config.json
+echo /mybin/config.json
+cat /mybin/config.json
+
+if [[ -z "${ProxySite}" ]]; then
+  s="s/proxy_pass/#proxy_pass/g"
+  echo "site:use local wwwroot html"
+else
+  s="s|\${ProxySite}|${ProxySite}|g"
+  echo "site: ${ProxySite}"
+fi
+
+sed -e "/^#/d"\
+    -e "s/\${PORT}/${PORT}/g"\
+    -e "s|\${Vless_Path}|${Vless_Path}|g"\
+    -e "s|\${Vmess_Path}|${Vmess_Path}|g"\
+    -e "s|\${Share_Path}|${Share_Path}|g"\
+    -e "$s"\
+    /conf/nginx.template.conf > /etc/nginx/conf.d/mybin.conf
+echo /etc/nginx/conf.d/mybin.conf
+cat /etc/nginx/conf.d/mybin.conf
+
+[ ! -d /wwwroot/${Share_Path} ] && mkdir -p /wwwroot/${Share_Path}
+sed -e "/^#/d"\
+    -e "s|\${_Vless_Path}|${Vless_Path}|g"\
+    -e "s|\${_Vmess_Path}|${Vmess_Path}|g"\
+    -e "s/\${_Vless_UUID}/${Vless_UUID}/g"\
+    -e "s/\${_Vmess_UUID}/${Vmess_UUID}/g"\
+    -e "$s"\
+    /conf/share.html > /wwwroot/${Share_Path}/index.html
+echo /wwwroot/${Share_Path}/index.html
+cat /wwwroot/${Share_Path}/index.html
+
+cd /mybin
+./mygo run -c ./config.json &
+rm -rf /etc/nginx/sites-enabled/default
+nginx -g 'daemon off;'
